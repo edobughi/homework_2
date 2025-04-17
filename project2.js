134146f53278472c2dfcbdbe19fadf1266525396
edobughi <168537499+edobughi@users.noreply.github.com>
2025-04-17 22:44:43 +0200

    Update project2.js

--- project2.js
+++ project2.js
@@ -3,7 +3,11 @@
 // The given rotation value is in degrees.
 function GetTransform( positionX, positionY, rotation, scale )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+	rad= rotation * Math.PI /180;
+	cos=Math.cos(rad);
+	sen=Math.sin(rad);
+
+	return Array( scale*cos, scale*sen, 0, -scale*sen, scale*cos, 0, positionX, positionY, 1 );
 }
 
 // Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
@@ -11,5 +15,15 @@ function GetTransform( positionX, positionY, rotation, scale )
 // The returned transformation first applies trans1 and then trans2.
 function ApplyTransform( trans1, trans2 )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+	let result=new Array(9);
+	for(let x=0;x<3;x++){
+		for(let y=0;y<3;y++){
+			let tot=0;
+			for(let k=0;k<3;k++){
+				tot+= trans2[k*3+x]* trans1[y*3 +k];
+			}
+			result[y*3+x]=tot;
+		}
+	}
+	return result;
 }
